0df02efa4d7dd73bf77a5b1f1dc3f62ef9c19cc8
Rrreert <96475871+Rrreert@users.noreply.github.com>
2023-07-30 18:55:07 +0800

    Update ICH_app.py

--- ICH_app.py
+++ ICH_app.py
@@ -90,7 +90,6 @@ with st.form("my_form"):
         with st.spinner('force plot generation, please wait...'):
             explainer = shap.TreeExplainer(model)
             shap_values = explainer.shap_values(test_df)
-            plt.figure(figsize=(24, 6))
             shap.force_plot(explainer.expected_value, shap_values[0], test_df.iloc[0].values, feature_names=['Use of anticoagulant drugs', 'Use of mannitol', 'Use of vasoactive drugs', 
                                                           'Mechanical ventilation', 'Temperature', 'Surgical intervention', 'Potassium', 
                                                           'SOFA', 'Blood oxygen saturation', 'RDW', 'Heart failure', 'Sodium', 'GCS', 'Chloride'], matplotlib=True, show=False)
