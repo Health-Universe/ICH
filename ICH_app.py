3900d74053f21919896eb94f011c8ae66ba8169f
Rrreert <96475871+Rrreert@users.noreply.github.com>
2023-07-30 18:24:30 +0800

    Update ICH_app.py

--- ICH_app.py
+++ ICH_app.py
@@ -5,6 +5,7 @@ from streamlit_option_menu import option_menu
 import datetime
 import pandas as pd
 import time
+import shap
 
 if 'model' not in st.session_state:
     model = joblib.load('model.pkl')
@@ -43,7 +44,7 @@ def set_background():
 
 
 set_background()
-st.markdown("<h1 style='text-align: center'>xxx system</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center'>Computational tool for predicting in-hospital mortality in severe spontaneous intracerebral hemorrhage</h1>", unsafe_allow_html=True)
 select_dic = {
     'No': 0,
     'Yes': 1
@@ -52,72 +53,47 @@ col1, col2, col3 = st.columns([2, 6, 2])
 with col1:
     st.image('logo.png')
 with col2:
-    st.markdown("<h2 style='text-align: center'>xxx system</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center'>Please answer the questions below to calculate:</h2>", unsafe_allow_html=True)
 with st.form("my_form"):
-    col9, col10, col11, col12 = st.columns([2.5, 2.5, 2.5, 2.5])
     col7, col8 = st.columns([5, 5])
-    with col9:
-        in_time = st.date_input(
-            "intime",
-            datetime.datetime.now())
-    with col10:
-        in_time_ = st.time_input("", datetime.time(8, 0), step=60)
     with col7:
-        potassium_mean = st.text_input("potassium_mean")
-        gcs_min = st.text_input("gcs_min")
-        sofa = st.text_input("sofa")
-        calcium_mean = st.text_input("calcium_mean")
-        spo2_mean = st.text_input("spo2_mean")
-        rdw_mean = st.text_input("rdw_mean")
-        sodium_mean = st.text_input("sodium_mean")
-    with col11:
-        out_time = st.date_input(
-            "outime",
-            datetime.datetime.now())
-    with col12:
-        out_time_ = st.time_input("", datetime.time(8, 0), key='444', step=60)
+        vaso_drug = st.selectbox('Use of vasoactive drugs', ('No', 'Yes'))
+        mannitol = st.selectbox('Use of mannitol', ('No', 'Yes'))
+        anticoagulants = st.selectbox('Use of anticoagulant drugs', ('No', 'Yes'))
+        ventilation = st.selectbox('Mechanical ventilation', ('No', 'Yes'))
+        heart_failure = st.selectbox('Heart failure', ('No', 'Yes'))
+        surgical_intervention = st.selectbox('Surgical intervention', ('No', 'Yes'))
+        chloride_mean = st.text_input("Chloride")
     with col8:
-        anticoagulants = st.selectbox('anticoagulants', ('No', 'Yes'))
-        mannitol = st.selectbox('mannitol', ('No', 'Yes'))
-        vaso_drug = st.selectbox('vaso_drug', ('No', 'Yes'))
-        ventilation = st.selectbox('ventilation', ('No', 'Yes'))
-        temperature_mean = st.text_input("temperature_mean")
-        surgical_intervention = st.selectbox('surgical_intervention', ('No', 'Yes'))
-        heart_failure = st.selectbox('heart_failure', ('No', 'Yes'))
-
+        gcs_min = st.text_input("GCS")
+        sofa = st.text_input("SOFA")
+        temperature_mean = st.text_input("Temperature(℃)")
+        rdw_mean = st.text_input("RDW(%)")
+        sodium_mean = st.text_input("Sodium(mmol/L)")
+        potassium_mean = st.text_input("Potassium(mmol/L)")
+        spo2_mean = st.text_input("Blood oxygen saturation(%)")
     submitted = st.form_submit_button("Calculate")
     if submitted:
-        flag = datetime.datetime.combine(out_time, out_time_) - datetime.datetime.combine(in_time, in_time_)
         test_df = pd.DataFrame(
             [select_dic[anticoagulants], select_dic[mannitol], select_dic[vaso_drug], select_dic[ventilation],
-             float(temperature_mean), select_dic[surgical_intervention], select_dic[heart_failure],
-             float(potassium_mean), int(gcs_min), int(sofa), float(calcium_mean), float(spo2_mean),
-             float(rdw_mean), float(sodium_mean), pd.Series(flag).dt.total_seconds()[0] / 86400]).T
-        st.subheader("Probability of disease occurrence: {:.3f}%".format(model.predict_proba(test_df)[0][1] * 100))
-col4, col5, col6 = st.columns([2, 6, 2])
-with col5:
-    selected_footer = option_menu(
-        menu_title=None,
-        options=["Project Instruction", "Model Description", "Model Description Figures", "Project Flow Chart"],
-        icons=["projector-fill", "info-circle-fill", "bar-chart-steps", "diagram-3-fill"],
-        default_index=0,
-        orientation="horizontal",
-    )
-    if selected_footer == "Project Instruction":
-        st.write('xxx')
-    elif selected_footer == "Model Description":
-        st.write('123')
-    elif selected_footer == "Model Description Figures":
-        sign = True
-        bar = st.empty()
-        em = st.empty()
-        img = st.empty()
-        while sign:
-            my_bar = bar.progress(0)
-            em.markdown('<div style="height: 16vh;width:20vw"></div>', unsafe_allow_html=True)
-            for i in range(1, 3):
-                my_bar.progress(i * 1 / 2)
-                img.image('{}.png'.format(i))
-                time.sleep(3)
-    elif selected_footer == "Project Flow Chart":
-        st.write('123')
+             float(temperature_mean), select_dic[surgical_intervention], float(potassium_mean), 
+             int(sofa), float(spo2_mean), float(rdw_mean), select_dic[heart_failure], float(sodium_mean), 
+             int(gcs_min), float(chloride_mean)]).T
+        pre_res = model.predict_proba(test_df)[0][1] * 100
+        if  pre_res> 29.3:
+            flag = 'Please early intervention and monitoring.'
+        elif pre_res < 29.3:
+            flag = 'Please continue the current effective treatment and closely monitor the patient's condition.'
+        st.subheader("The probability of being hospitalized and dying from the disease is {:.3f}%（the threshold for  disease mortality is 29.3%）".format(pre_res))
+        st.subheader(flag)
+
+        explainer = shap.TreeExplainer(model)
+        shap_values = explainer.shap_values(test_df.iloc[0].values)
+        shap.force_plot(explainer.expected_value, shap_values[0], test_df.iloc[0].values, feature_names=['Use of anticoagulant drugs', 'Use of mannitol', 'Use of vasoactive drugs', 
+                                                      'Mechanical ventilation', 'Temperature', 'Surgical intervention', 'Potassium', 
+                                                      'SOFA', 'Blood oxygen saturation', 'RDW', 'Heart failure', 'Sodium', 'GCS', 'Chloride'], matplotlib=True, show=False)
+        plt.xticks(fontproperties='Times New Roman', size=16)
+        plt.yticks(fontproperties='Times New Roman', size=20)
+        plt.tight_layout()
+        plt.savefig('force.png', dpi=600)
+        st.image('force.png')
